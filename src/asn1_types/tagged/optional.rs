efe71b0f9fe4bec22a051386d5dee6368b52323d
Pierre Chifflier <chifflier@wzdftpd.net>
2021-07-19 15:09:10 +0200

    Tagged values: continue experiments in const generics to store tag

--- src/asn1_types/tagged/optional.rs
+++ src/asn1_types/tagged/optional.rs
@@ -1,5 +1,9 @@
-use crate::{Any, Class, Error, FromBer, FromDer, Header, ParseResult, Tag};
+use crate::{
+    Any, CheckDerConstraints, Class, Error, Explicit, FromBer, FromDer, Header, Implicit,
+    ParseResult, Tag, Tagged,
+};
 use core::convert::TryFrom;
+use std::marker::PhantomData;
 
 pub const TAG_OPT0: TaggedOptional = TaggedOptional::new(Class::ContextSpecific, Tag(0));
 pub const TAG_OPT1: TaggedOptional = TaggedOptional::new(Class::ContextSpecific, Tag(1));
@@ -166,66 +170,244 @@ impl From<u32> for TaggedOptional {
 
 //
 // XXX 2nd try, with a const generic to contain the tag number (class ?)
+// this allows directly returning an object with TryFrom<Any>
 
-#[derive(Debug)]
-pub struct TaggedOptionalExplicit<T, const N: u32> {
-    pub(crate) inner: Option<T>,
+/// Helper object for creating `FromBer`/`FromDer` types for TAGGED OPTIONAL types
+///
+/// When parsing `ContextSpecific` (most common class), see [`TaggedExplicit`] and
+/// [`TaggedImplicit`] alias types.
+///
+/// # Examples
+///
+/// To parse a `[APPLICATION 0] EXPLICIT INTEGER` object:
+///
+/// ```rust
+/// use asn1_rs::{Explicit, FromBer, Integer, TaggedParser};
+///
+/// let bytes = &[0x60, 0x03, 0x2, 0x1, 0x2];
+///
+/// // If tagged object is present (and has expected tag), parsing succeeds:
+/// let (_, tagged) = TaggedParser::<Integer, Explicit, 0b01, 0>::from_ber(bytes).unwrap();
+/// assert_eq!(tagged, TaggedParser::explicit(Integer::from(2)));
+/// ```
+#[derive(Debug, PartialEq)]
+pub struct TaggedParser<T, TagKind, const CLASS: u8, const TAG: u32> {
+    pub(crate) inner: T,
+
+    tag_kind: PhantomData<TagKind>,
 }
 
-impl<T, const N: u32> AsRef<Option<T>> for TaggedOptionalExplicit<T, N> {
-    fn as_ref(&self) -> &Option<T> {
+impl<T, TagKind, const CLASS: u8, const TAG: u32> TaggedParser<T, TagKind, CLASS, TAG> {
+    /// Consumes the `TaggedParser`, returning the wrapped value.
+    #[inline]
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
+}
+
+impl<T, const CLASS: u8, const TAG: u32> TaggedParser<T, Explicit, CLASS, TAG> {
+    /// Constructs a new `EXPLICIT TaggedParser` with the provided value
+    #[inline]
+    pub const fn explicit(inner: T) -> Self {
+        TaggedParser {
+            inner,
+            tag_kind: PhantomData,
+        }
+    }
+}
+
+impl<T, const CLASS: u8, const TAG: u32> TaggedParser<T, Implicit, CLASS, TAG> {
+    /// Constructs a new `IMPLICIT TaggedParser` with the provided value
+    #[inline]
+    pub const fn implicit(inner: T) -> Self {
+        TaggedParser {
+            inner,
+            tag_kind: PhantomData,
+        }
+    }
+}
+
+impl<T, TagKind, const CLASS: u8, const TAG: u32> AsRef<T>
+    for TaggedParser<T, TagKind, CLASS, TAG>
+{
+    fn as_ref(&self) -> &T {
         &self.inner
     }
 }
 
-impl<'a, T, const N: u32> TryFrom<Any<'a>> for TaggedOptionalExplicit<T, N>
+impl<'a, T, const CLASS: u8, const TAG: u32> TryFrom<Any<'a>>
+    for TaggedParser<T, Explicit, CLASS, TAG>
 where
     T: FromBer<'a>,
 {
     type Error = Error;
 
     fn try_from(any: Any<'a>) -> Result<Self, Self::Error> {
-        if any.tag().0 != N {
-            Ok(TaggedOptionalExplicit { inner: None })
-        } else {
-            let (_, t) = T::from_ber(any.data)?;
-            Ok(TaggedOptionalExplicit { inner: Some(t) })
+        any.tag().assert_eq(Tag(TAG))?;
+        any.header.assert_constructed()?;
+        if any.class() as u8 != CLASS {
+            return Err(Error::UnexpectedClass(any.class()));
         }
+        let (_, inner) = T::from_ber(any.data)?;
+        Ok(TaggedParser::explicit(inner))
     }
 }
 
-#[derive(Debug)]
-pub struct TaggedOptionalImplicit<T, const N: u32> {
-    pub(crate) inner: Option<T>,
-}
-
-impl<T, const N: u32> AsRef<Option<T>> for TaggedOptionalImplicit<T, N> {
-    fn as_ref(&self) -> &Option<T> {
-        &self.inner
+impl<'a, T, const CLASS: u8, const TAG: u32> CheckDerConstraints
+    for TaggedParser<T, Explicit, CLASS, TAG>
+where
+    T: CheckDerConstraints,
+{
+    fn check_constraints(any: &Any) -> crate::Result<()> {
+        any.header.length.assert_definite()?;
+        let (_, inner) = Any::from_ber(&any.data)?;
+        T::check_constraints(&inner)?;
+        Ok(())
     }
 }
 
-impl<'a, T, const N: u32> TryFrom<Any<'a>> for TaggedOptionalImplicit<T, N>
+impl<'a, T, const CLASS: u8, const TAG: u32> TryFrom<Any<'a>>
+    for TaggedParser<T, Implicit, CLASS, TAG>
 where
     T: TryFrom<Any<'a>, Error = Error>,
+    T: Tagged,
 {
     type Error = Error;
 
     fn try_from(any: Any<'a>) -> Result<Self, Self::Error> {
-        if any.tag().0 != N {
-            Ok(TaggedOptionalImplicit { inner: None })
-        } else {
-            let any = Any {
-                header: Header {
-                    tag: Tag(N),
-                    ..any.header.clone()
-                },
-                data: any.data,
-            };
-            match T::try_from(any) {
-                Ok(t) => Ok(TaggedOptionalImplicit { inner: Some(t) }),
-                Err(e) => Err(e),
-            }
+        any.tag().assert_eq(Tag(TAG))?;
+        // XXX if input is empty, this function is not called
+
+        if any.class() as u8 != CLASS {
+            return Err(Error::UnexpectedClass(any.class()));
+        }
+        let any = Any {
+            header: Header {
+                tag: T::TAG,
+                ..any.header.clone()
+            },
+            data: any.data,
+        };
+        match T::try_from(any) {
+            Ok(inner) => Ok(TaggedParser::implicit(inner)),
+            Err(e) => Err(e),
         }
     }
 }
+
+impl<'a, T, const CLASS: u8, const TAG: u32> CheckDerConstraints
+    for TaggedParser<T, Implicit, CLASS, TAG>
+where
+    T: CheckDerConstraints,
+    T: Tagged,
+{
+    fn check_constraints(any: &Any) -> crate::Result<()> {
+        any.header.length.assert_definite()?;
+        let header = any.header.clone().with_tag(T::TAG);
+        let inner = Any::new(header, any.data);
+        T::check_constraints(&inner)?;
+        Ok(())
+    }
+}
+
+const CONTEXT_SPECIFIC: u8 = Class::ContextSpecific as u8;
+
+/// A helper object to parse `[ 0 ] EXPLICIT T`
+///
+/// A helper object implementing [`FromBer`] and [`FromDer`], to parse tagged
+/// optional values.
+///
+/// This helper expects context-specific tags.
+/// Use [`TaggedParser`] for a more generic implementation.
+///
+/// # Examples
+///
+/// To parse a `[0] EXPLICIT INTEGER` object:
+///
+/// ```rust
+/// use asn1_rs::{FromBer, Integer, TaggedExplicit, TaggedParser};
+///
+/// let bytes = &[0xa0, 0x03, 0x2, 0x1, 0x2];
+///
+/// // If tagged object is present (and has expected tag), parsing succeeds:
+/// let (_, tagged) = TaggedExplicit::<Integer, 0>::from_ber(bytes).unwrap();
+/// assert_eq!(tagged, TaggedParser::explicit(Integer::from(2)));
+/// ```
+pub type TaggedExplicit<T, const TAG: u32> = TaggedParser<T, Explicit, CONTEXT_SPECIFIC, TAG>;
+
+/// A helper object to parse `[ n ] EXPLICIT T OPTIONAL`
+///
+/// A helper object implementing [`FromBer`] and [`FromDer`], to parse tagged
+/// optional values.
+///
+/// This helper expects context-specific tags.
+/// Use `Option<` [`TaggedParser`] `>` for a more generic implementation.
+///
+/// # Examples
+///
+/// To parse a `[0] EXPLICIT INTEGER OPTIONAL` object:
+///
+/// ```rust
+/// use asn1_rs::{FromBer, Integer, OptTaggedExplicit, TaggedParser};
+///
+/// let bytes = &[0xa0, 0x03, 0x2, 0x1, 0x2];
+///
+/// // If tagged object is present (and has expected tag), parsing succeeds:
+/// let (_, tagged) = OptTaggedExplicit::<Integer, 0>::from_ber(bytes).unwrap();
+/// assert_eq!(tagged, Some(TaggedParser::explicit(Integer::from(2))));
+///
+/// // If tagged object is not present or has different tag, parsing
+/// // also succeeds (returning None):
+/// let (_, tagged) = OptTaggedExplicit::<Integer, 0>::from_ber(&[]).unwrap();
+/// assert_eq!(tagged, None);
+/// ```
+pub type OptTaggedExplicit<T, const TAG: u32> = Option<TaggedExplicit<T, TAG>>;
+
+/// A helper object to parse `[ n ] IMPLICIT T`
+///
+/// A helper object implementing [`FromBer`] and [`FromDer`], to parse tagged
+/// optional values.
+///
+/// This helper expects context-specific tags.
+/// Use `Option<` [`TaggedParser`] `>` for a more generic implementation.
+///
+/// # Examples
+///
+/// To parse a `[0] IMPLICIT INTEGER OPTIONAL` object:
+///
+/// ```rust
+/// use asn1_rs::{FromBer, Integer, TaggedImplicit, TaggedParser};
+///
+/// let bytes = &[0xa0, 0x1, 0x2];
+///
+/// let (_, tagged) = TaggedImplicit::<Integer, 0>::from_ber(bytes).unwrap();
+/// assert_eq!(tagged, TaggedParser::implicit(Integer::from(2)));
+/// ```
+pub type TaggedImplicit<T, const TAG: u32> = TaggedParser<T, Implicit, CONTEXT_SPECIFIC, TAG>;
+
+/// A helper object to parse `[ n ] IMPLICIT T OPTIONAL`
+///
+/// A helper object implementing [`FromBer`] and [`FromDer`], to parse tagged
+/// optional values.
+///
+/// This helper expects context-specific tags.
+/// Use `Option<` [`TaggedParser`] `>` for a more generic implementation.
+///
+/// # Examples
+///
+/// To parse a `[0] IMPLICIT INTEGER OPTIONAL` object:
+///
+/// ```rust
+/// use asn1_rs::{FromBer, Integer, OptTaggedImplicit, TaggedParser};
+///
+/// let bytes = &[0xa0, 0x1, 0x2];
+///
+/// let (_, tagged) = OptTaggedImplicit::<Integer, 0>::from_ber(bytes).unwrap();
+/// assert_eq!(tagged, Some(TaggedParser::implicit(Integer::from(2))));
+///
+/// // If tagged object is not present or has different tag, parsing
+/// // also succeeds (returning None):
+/// let (_, tagged) = OptTaggedImplicit::<Integer, 0>::from_ber(&[]).unwrap();
+/// assert_eq!(tagged, None);
+/// ```
+pub type OptTaggedImplicit<T, const TAG: u32> = Option<TaggedImplicit<T, TAG>>;
