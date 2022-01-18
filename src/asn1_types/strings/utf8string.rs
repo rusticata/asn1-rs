c70cdc1b8388e8530f3b491134353f1f21234844
Pierre Chifflier <chifflier@wzdftpd.net>
2022-01-18 09:19:25 +0100

    Fix build with no_std

--- src/asn1_types/strings/utf8string.rs
+++ src/asn1_types/strings/utf8string.rs
@@ -7,7 +7,7 @@ asn1_string!(Utf8String);
 
 impl<'a> TestValidCharset for Utf8String<'a> {
     fn test_valid_charset(i: &[u8]) -> Result<()> {
-        let _ = std::str::from_utf8(i)?;
+        let _ = core::str::from_utf8(i)?;
         Ok(())
     }
 }
