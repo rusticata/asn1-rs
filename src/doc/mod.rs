fbfe4de05be9fcf5f10753356ea25a751b59f4e2
Pierre Chifflier <chifflier@wzdftpd.net>
2022-02-28 14:20:36 +0100

    Move documentation files to the `doc` directory

--- src/doc/mod.rs
+++ src/doc/mod.rs
@@ -1,4 +1,4 @@
 //! Additional documentation: recipes, specific use cases and examples, etc.
 
-#[doc = include_str!("../../RECIPES.md")]
+#[doc = include_str!("../../doc/RECIPES.md")]
 pub mod recipes {}
