1ca99815afd65ad23bb9f95d2ee5d0de451a3494
Pierre Chifflier <chifflier@wzdftpd.net>
2024-02-21 13:59:32 +0100

    Option: require T::Tagged, and check tag before constraints (Closes #27)

--- src/asn1_types/optional.rs
+++ src/asn1_types/optional.rs
@@ -8,14 +8,20 @@ use crate::*;
 impl<'a, T> FromBer<'a> for Option<T>
 where
     T: FromBer<'a>,
+    T: Tagged,
 {
     fn from_ber(bytes: &'a [u8]) -> ParseResult<Self> {
         if bytes.is_empty() {
             return Ok((bytes, None));
         }
+        if let Ok((_, header)) = Header::from_ber(bytes) {
+            if T::TAG != header.tag {
+                // not the expected tag, early return
+                return Ok((bytes, None));
+            }
+        }
         match T::from_ber(bytes) {
             Ok((rem, t)) => Ok((rem, Some(t))),
-            Err(nom::Err::Error(Error::UnexpectedTag { .. })) => Ok((bytes, None)),
             Err(e) => Err(e),
         }
     }
@@ -24,14 +30,20 @@ where
 impl<'a, T> FromDer<'a> for Option<T>
 where
     T: FromDer<'a>,
+    T: Tagged,
 {
     fn from_der(bytes: &'a [u8]) -> ParseResult<Self> {
         if bytes.is_empty() {
             return Ok((bytes, None));
         }
+        if let Ok((_, header)) = Header::from_der(bytes) {
+            if T::TAG != header.tag {
+                // not the expected tag, early return
+                return Ok((bytes, None));
+            }
+        }
         match T::from_der(bytes) {
             Ok((rem, t)) => Ok((rem, Some(t))),
-            Err(nom::Err::Error(Error::UnexpectedTag { .. })) => Ok((bytes, None)),
             Err(e) => Err(e),
         }
     }
