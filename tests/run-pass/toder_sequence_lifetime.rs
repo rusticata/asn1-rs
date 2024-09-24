2171df06061198b831c8182ea64d674b43dd424a
Pierre Chifflier <chifflier@wzdftpd.net>
2024-09-24 15:34:43 +0200

    ToDerSequence: add support for struct lifetime

--- tests/run-pass/toder_sequence_lifetime.rs
+++ tests/run-pass/toder_sequence_lifetime.rs
@@ -3,17 +3,26 @@ use hex_literal::hex;
 
 #[derive(Debug, PartialEq, DerSequence, ToDerSequence)]
 //#[debug_derive]
-pub struct T1 {
+pub struct T1<'a> {
     a: u32,
     b: u16,
     c: u16,
+    d: &'a str,
 }
 
 fn main() {
-    let input = &hex!("30090201 01020102 020103");
+    let input = &hex!("300f0201 01020102 020103 0c0461626364");
     let (rem, t1) = T1::from_der(input).expect("parsing failed");
     assert!(rem.is_empty());
-    assert_eq!(t1, T1 { a: 1, b: 2, c: 3 });
+    assert_eq!(
+        t1,
+        T1 {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: "abcd"
+        }
+    );
     // serialize back data
     let output = t1.to_der_vec().expect("serialization failed");
     assert_eq!(&input[..], output);
