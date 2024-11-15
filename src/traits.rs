1e7184a1dc5ec0f9d75ed899b0f1888c93b15cbc
Pierre Chifflier <chifflier@wzdftpd.net>
2024-11-15 11:05:41 +0100

    Implement ToStatic for primitive types

--- src/traits.rs
+++ src/traits.rs
@@ -1,8 +1,6 @@
 use crate::debug::{trace, trace_generic};
 use crate::error::*;
 use crate::{parse_der_any, Any, Class, Explicit, Implicit, Tag, TaggedParser};
-use alloc::borrow::Cow;
-use alloc::string::ToString;
 use core::convert::{TryFrom, TryInto};
 use core::fmt::{Debug, Display};
 #[cfg(feature = "std")]
@@ -353,21 +351,4 @@ pub trait AsTaggedImplicit<'a, E = Error>: Sized {
 
 impl<'a, T, E> AsTaggedImplicit<'a, E> for T where T: Sized + 'a {}
 
-pub trait ToStatic {
-    type Owned: 'static;
-    fn to_static(&self) -> Self::Owned;
-}
-
-impl ToStatic for Cow<'_, str> {
-    type Owned = Cow<'static, str>;
-    fn to_static(&self) -> <Self as ToStatic>::Owned {
-        Cow::Owned(self.to_string())
-    }
-}
-
-impl ToStatic for Cow<'_, [u8]> {
-    type Owned = Cow<'static, [u8]>;
-    fn to_static(&self) -> <Self as ToStatic>::Owned {
-        Cow::Owned(self.to_vec())
-    }
-}
+pub use crate::tostatic::*;
---
+++ tests/to_static.rs
@@ -0,0 +1,62 @@
+use std::borrow::Cow;
+
+use asn1_rs::*;
+
+#[derive(ToStatic)]
+pub struct Unit;
+
+#[test]
+fn derive_unit_tostatic() {
+    let unit = Unit;
+
+    let _static = unit.to_static();
+    assert_static_lifetime(&unit);
+}
+
+#[derive(ToStatic)]
+pub struct Unnamed<'a>(pub Cow<'a, str>);
+
+#[test]
+fn derive_unnamed_tostatic() {
+    let s = Cow::Borrowed("test");
+    let unnamed = Unnamed(s);
+
+    let _static = unnamed.to_static();
+    assert!(matches! { _static.0, Cow::Owned(_) });
+}
+
+#[derive(ToStatic)]
+pub struct Named<'a> {
+    cow: Cow<'a, str>,
+}
+
+#[derive(ToStatic)]
+//#[debug_derive]
+pub struct Embed<'a> {
+    a: Cow<'a, str>,
+    n: Named<'a>,
+}
+
+#[test]
+fn derive_named_tostatic() {
+    let s = Cow::Borrowed("test");
+    let named1 = Named { cow: s };
+
+    let _static1 = named1.to_static();
+    assert_static_lifetime(&_static1);
+    assert!(matches! { _static1.cow, Cow::Owned(_) });
+
+    let s2 = Cow::Borrowed("test2");
+    let named2 = Embed { a: s2, n: named1 };
+
+    let _static2 = named2.to_static();
+    assert_static_lifetime(&_static2);
+    assert!(matches! { _static2.a, Cow::Owned(_) });
+    assert!(matches! { _static2.n.cow, Cow::Owned(_) });
+}
+
+fn assert_static_lifetime<T>(_arg: &T)
+where
+    T: 'static,
+{
+}
