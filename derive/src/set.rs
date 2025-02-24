928c9ff457342418e42c8aa3c9e91afa5d8d6227
Pierre Chifflier <chifflier@wzdftpd.net>
2025-02-24 17:55:12 +0100

    Derive: factorize code for Sequence/Set

--- derive/src/set.rs
+++ derive/src/set.rs
@@ -1,143 +1,18 @@
-use crate::{check_derive::check_lastderive_fromber, container::*};
-use proc_macro2::Span;
-use quote::quote;
-use syn::{Data, Ident};
+use crate::container::*;
+use crate::sequence::*;
 
 pub fn derive_ber_set(s: synstructure::Structure) -> proc_macro2::TokenStream {
-    let ast = s.ast();
-
-    let container = match &ast.data {
-        Data::Struct(ds) => Container::from_datastruct(ds, ast, ContainerType::Set),
-        _ => panic!("Unsupported type, cannot derive"),
-    };
-
-    let debug_derive = ast.attrs.iter().any(|attr| {
-        attr.meta
-            .path()
-            .is_ident(&Ident::new("debug_derive", Span::call_site()))
-    });
-
-    let last_berderive = check_lastderive_fromber(ast);
-
-    let impl_tryfrom = container.gen_tryfrom();
-    let impl_tagged = if last_berderive {
-        container.gen_tagged()
-    } else {
-        quote! {}
-    };
-    let ts = s.gen_impl(quote! {
-        extern crate asn1_rs;
-
-        #impl_tryfrom
-        #impl_tagged
-    });
-    if debug_derive {
-        eprintln!("{}", ts);
-    }
-    ts
+    derive_ber_container(s, ContainerType::Set)
 }
 
 pub fn derive_berparser_set(s: synstructure::Structure) -> proc_macro2::TokenStream {
-    let ast = s.ast();
-
-    let container = match &ast.data {
-        Data::Struct(ds) => Container::from_datastruct(ds, ast, ContainerType::Set),
-        _ => panic!("Unsupported type, cannot derive"),
-    };
-
-    let debug_derive = ast.attrs.iter().any(|attr| {
-        attr.meta
-            .path()
-            .is_ident(&Ident::new("debug_derive", Span::call_site()))
-    });
-
-    let last_berderive = check_lastderive_fromber(ast);
-
-    let impl_tagged = if last_berderive {
-        container.gen_tagged()
-    } else {
-        quote! {}
-    };
-    let impl_berparser = container.gen_berparser();
-    let ts = s.gen_impl(quote! {
-        extern crate asn1_rs;
-
-        #impl_tagged
-        #impl_berparser
-    });
-    if debug_derive {
-        eprintln!("{}", ts);
-    }
-    ts
+    derive_berparser_container(s, ContainerType::Set)
 }
 
 pub fn derive_der_set(s: synstructure::Structure) -> proc_macro2::TokenStream {
-    let ast = s.ast();
-
-    let container = match &ast.data {
-        Data::Struct(ds) => Container::from_datastruct(ds, ast, ContainerType::Set),
-        _ => panic!("Unsupported type, cannot derive"),
-    };
-
-    let debug_derive = ast.attrs.iter().any(|attr| {
-        attr.meta
-            .path()
-            .is_ident(&Ident::new("debug_derive", Span::call_site()))
-    });
-    let last_berderive = check_lastderive_fromber(ast);
-
-    let impl_tagged = if last_berderive {
-        container.gen_tagged()
-    } else {
-        quote! {}
-    };
-    let impl_tryfrom = container.gen_tryfrom();
-    let impl_checkconstraints = container.gen_checkconstraints();
-    let impl_fromder = container.gen_fromder();
-    let ts = s.gen_impl(quote! {
-        extern crate asn1_rs;
-
-        #impl_tryfrom
-        #impl_tagged
-        #impl_checkconstraints
-        #impl_fromder
-    });
-    if debug_derive {
-        eprintln!("{}", ts);
-    }
-    ts
+    derive_der_container(s, ContainerType::Set)
 }
 
 pub fn derive_derparser_set(s: synstructure::Structure) -> proc_macro2::TokenStream {
-    let ast = s.ast();
-
-    let container = match &ast.data {
-        Data::Struct(ds) => Container::from_datastruct(ds, ast, ContainerType::Set),
-        _ => panic!("Unsupported type, cannot derive"),
-    };
-
-    let debug_derive = ast.attrs.iter().any(|attr| {
-        attr.meta
-            .path()
-            .is_ident(&Ident::new("debug_derive", Span::call_site()))
-    });
-
-    let last_berderive = check_lastderive_fromber(ast);
-
-    let impl_tagged = if last_berderive {
-        container.gen_tagged()
-    } else {
-        quote! {}
-    };
-    let impl_derparser = container.gen_derparser();
-    let ts = s.gen_impl(quote! {
-        extern crate asn1_rs;
-
-        #impl_tagged
-        #impl_derparser
-    });
-    if debug_derive {
-        eprintln!("{}", ts);
-    }
-    ts
+    derive_derparser_container(s, ContainerType::Set)
 }
