e734ab7a96004a6250e40bea5a6b006b628199e8
Pierre Chifflier <chifflier@wzdftpd.net>
2025-02-28 09:18:51 +0100

    Refactor Ber/DerParser for EmbeddedPdv, and add warning (no test data)

--- src/asn1_types/embedded_pdv.rs
+++ src/asn1_types/embedded_pdv.rs
@@ -1,6 +1,15 @@
 use crate::*;
 use core::convert::TryFrom;
 
+/// EMBEDDED PDV ASN.1 Object
+///
+/// <div class="warning">Warning: this implementation has not been tested
+///
+/// Due to lack of other implementations and of testing data, the parsers for this object have
+/// **not** been tested with real data. If you have such data, please contact us (for ex, open
+/// an issue).
+///
+/// </div>
 #[derive(Debug, PartialEq, Eq)]
 pub struct EmbeddedPdv<'a> {
     pub identification: PdvIdentification<'a>,
@@ -117,7 +126,112 @@ impl<'a, 'b> TryFrom<&'b Any<'a>> for EmbeddedPdv<'a> {
     }
 }
 
-impl DeriveBerParserFromTryFrom for EmbeddedPdv<'_> {}
+impl<'i> BerParser<'i> for EmbeddedPdv<'i> {
+    type Error = BerError<Input<'i>>;
+
+    fn check_tag(tag: Tag) -> bool {
+        tag == Tag::EmbeddedPdv
+    }
+
+    fn from_any_ber(
+        input: Input<'i>,
+        _header: Header<'i>,
+    ) -> IResult<Input<'i>, Self, Self::Error> {
+        // AUTOMATIC TAGS means all values will be tagged (IMPLICIT)
+        // definition taken from <https://www.oss.com/asn1/products/documentation/asn1_java_8.7/api/toed/com/oss/asn1/EmbeddedPDV.html>
+
+        // [0] -> identification
+        // this is a [0 IMPLICIT] SEQUENCE, so we can just parse it as Any and take the content
+        let (rem, t0) = TaggedImplicit::<Any, Self::Error, 0>::parse_ber(input)?;
+        let inner0 = t0.into_inner();
+        let identification = match inner0.tag() {
+            Tag(0) => {
+                // syntaxes SEQUENCE {
+                //     abstract OBJECT IDENTIFIER,
+                //     transfer OBJECT IDENTIFIER
+                // },
+                let inner_data = inner0.data;
+                let (rem, t_abstract) =
+                    TaggedImplicit::<Oid, Self::Error, 0>::parse_ber(inner_data)?;
+                let (_, t_transfer) = TaggedImplicit::<Oid, Self::Error, 1>::parse_ber(rem)?;
+                PdvIdentification::Syntaxes {
+                    s_abstract: t_abstract.into_inner(),
+                    s_transfer: t_transfer.into_inner(),
+                }
+            }
+            Tag(1) => {
+                // syntax OBJECT IDENTIFIER
+                let (_, oid) = Oid::from_any_ber(inner0.data, inner0.header)?;
+                PdvIdentification::Syntax(oid)
+            }
+            Tag(2) => {
+                // presentation-context-id INTEGER
+                let (_, i) = Integer::from_any_ber(inner0.data, inner0.header)?;
+                PdvIdentification::PresentationContextId(i)
+            }
+            Tag(3) => {
+                // context-negotiation SEQUENCE {
+                //     presentation-context-id INTEGER,
+                //     transfer-syntax OBJECT IDENTIFIER
+                // },
+                let inner_data = inner0.data;
+                let (rem, t_presentation_context_id) =
+                    TaggedImplicit::<Integer, Self::Error, 0>::parse_ber(inner_data)?;
+                let (_, t_presentation_syntax) =
+                    TaggedImplicit::<Oid, Self::Error, 1>::parse_ber(rem)?;
+
+                let presentation_context_id = t_presentation_context_id.into_inner();
+                let presentation_syntax = t_presentation_syntax.into_inner();
+                PdvIdentification::ContextNegotiation {
+                    presentation_context_id,
+                    presentation_syntax,
+                }
+            }
+            Tag(4) => {
+                // transfer-syntax OBJECT IDENTIFIER
+                let (_, oid) = Oid::from_any_ber(inner0.data, inner0.header)?;
+                PdvIdentification::TransferSyntax(oid)
+            }
+            Tag(5) => {
+                // fixed NULL
+                PdvIdentification::Fixed
+            }
+            _ => {
+                let e = InnerError::invalid_value(
+                    inner0.tag(),
+                    "Invalid identification tag in EMBEDDED PDV",
+                );
+                return Err(BerError::nom_err_input(&inner0.data, e));
+            }
+        };
+        // [1] -> data-value-descriptor ObjectDescriptor OPTIONAL
+        // *BUT* WITH COMPONENTS data-value-descriptor ABSENT
+        let (rem, t1) = <Option<TaggedImplicit<ObjectDescriptor, Self::Error, 1>>>::parse_ber(rem)?;
+        let data_value_descriptor = t1.map(|o| o.into_inner());
+        // [2] -> data-value OCTET STRING
+        let (rem, t2) = TaggedImplicit::<&[u8], Self::Error, 2>::parse_ber(rem)?;
+        let data_value = t2.into_inner();
+
+        let obj = EmbeddedPdv {
+            identification,
+            data_value_descriptor,
+            data_value,
+        };
+        Ok((rem, obj))
+    }
+}
+
+impl<'i> DerParser<'i> for EmbeddedPdv<'i> {
+    type Error = BerError<Input<'i>>;
+
+    fn check_tag(tag: Tag) -> bool {
+        tag == Tag::EmbeddedPdv
+    }
+
+    fn from_any_der(input: Input<'i>, header: Header<'i>) -> IResult<Input<'i>, Self, Self::Error> {
+        Self::from_any_ber(input, header)
+    }
+}
 
 impl CheckDerConstraints for EmbeddedPdv<'_> {
     fn check_constraints(any: &Any) -> Result<()> {
