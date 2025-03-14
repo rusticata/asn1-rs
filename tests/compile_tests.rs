91a72b980dfc75d2cf23665b19a31c1cf6f90b5f
Pierre Chifflier <chifflier@wzdftpd.net>
2025-03-14 11:28:10 +0100

    Split compile tests to reduce duration of tests and make debugging errors easier

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -1,6 +1,36 @@
 #[test]
+fn compile_pass_berparser() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/berparser*.rs");
+}
+
+#[test]
+fn compile_pass_derparser() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/derparser*.rs");
+}
+
+#[test]
+fn compile_pass_tober() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/tober*.rs");
+}
+
+#[test]
+fn compile_pass_toder() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/toder*.rs");
+}
+
+#[test]
+fn compile_pass_misc() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/run-pass/custom*.rs");
+    t.pass("tests/run-pass/der_*.rs");
+}
+
+#[test]
 fn compile_fail() {
     let t = trybuild::TestCases::new();
-    t.pass("tests/run-pass/*.rs");
     t.compile_fail("tests/compile-fail/*.rs");
 }
