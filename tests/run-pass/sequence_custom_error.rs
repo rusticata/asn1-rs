a3dd04783826492ff579c77caca5261cbc63a874
Pierre Chifflier <chifflier@wzdftpd.net>
2025-03-24 10:29:16 +0100

    Update docs/derive for custom errors, and add unit tests

---
+++ tests/run-pass/sequence_custom_error.rs
@@ -0,0 +1,78 @@
+use asn1_rs::*;
+use hex_literal::hex;
+
+#[derive(Debug, PartialEq)]
+pub enum MyError {
+    NotYetImplemented,
+}
+
+impl From<BerError<Input<'_>>> for MyError {
+    fn from(_: BerError<Input>) -> Self {
+        MyError::NotYetImplemented
+    }
+}
+
+impl nom::error::ParseError<Input<'_>> for MyError {
+    fn from_error_kind(_: Input, _: nom::error::ErrorKind) -> Self {
+        MyError::NotYetImplemented
+    }
+
+    fn append(_: Input, _: nom::error::ErrorKind, _: Self) -> Self {
+        MyError::NotYetImplemented
+    }
+}
+
+#[derive(Debug, PartialEq, Sequence)]
+#[error(MyError)]
+// #[debug_derive]
+pub struct T2 {
+    pub a: u32,
+}
+
+fn derive_sequence_custom_error() {
+    // Ok: sequence with expected content
+    let input = Input::from_slice(&hex!("30 04 020200aa"));
+    let (rem, res) = T2::parse_ber(input).expect("parsing failed");
+    assert!(rem.is_empty());
+    assert_eq!(res, T2 { a: 0xaa });
+
+    // Fail: not constructed
+    let input = Input::from_slice(&hex!("10 04 020200aa"));
+    let err = T2::parse_ber(input).expect_err("not constructed");
+    assert_eq!(err, Err::Error(MyError::NotYetImplemented));
+
+    // Fail: not a sequence
+    let input = Input::from_slice(&hex!("31 04 020200aa"));
+    let err = T2::parse_ber(input).expect_err("not a sequence");
+    assert_eq!(err, Err::Error(MyError::NotYetImplemented));
+}
+
+fn derive_sequence_map_err() {
+    // subparser returns an error of type MyError,
+    // in this example we map this to a `BerError`
+    #[derive(Debug, PartialEq, Sequence)]
+    pub struct T4 {
+        #[map_err(|_| BerError::new(Input::from(&[]), InnerError::Unsupported))]
+        pub t2: T2,
+    }
+
+    // Ok: sequence with expected content
+    let input = Input::from_slice(&hex!("30 06 30 04 020200aa"));
+    let (rem, res) = T4::parse_ber(input).expect("parsing failed");
+    assert!(rem.is_empty());
+    assert_eq!(res, T4 { t2: T2 { a: 0xaa } });
+
+    // Fail: inner object not a T2 sequence
+    let input = Input::from_slice(&hex!("30 04 020200aa"));
+    let err = T4::parse_ber(input).expect_err("not a sequence");
+    if let Err::Error(e) = err {
+        assert_eq!(*e.inner(), InnerError::Unsupported);
+    } else {
+        panic!("unexpected nom error type");
+    }
+}
+
+fn main() {
+    derive_sequence_custom_error();
+    derive_sequence_map_err();
+}
